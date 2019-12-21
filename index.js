e3c7f77bf4a6e9a73c0c5f789568daeb81daa8a6
Claire Marchal <clairemarchal@mbp-de-claire.home>
2019-12-21 21:54:23 +0100

    heroku maj

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config(); // Permet d'activer les variables d'environnement qui se trouvent dans le fichier `.env`
 const express = require("express");
 const mongoose = require("mongoose");
 const formidableMiddleware = require("express-formidable");
@@ -19,12 +20,20 @@ const backOfficeRoutes = require("./routes/backOffice");
 app.use(backOfficeRoutes);
 
 // on se connecte à la base de données : *******************************
-mongoose.connect("mongodb://localhost/certif", {
+// mongoose.connect("mongodb://localhost/certif", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+// mongoose.connect(process.env.MONGODB_URI);
+
 //on écoute les appels de l'extérieur sur le port 4001 : ********************************************
-app.listen(4001, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server Started");
 });
+
+//app.listen(process.env.PORT);
